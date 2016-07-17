3c5815af27fb6ec0a8025e577a1d184d33c071a4
shaowenhan <1138500211@qq.com>
2016-07-17 12:03:30 +0800

    add valid number testcase with e and finish this function

--- ValidNumber/test_solution.py
+++ ValidNumber/test_solution.py
@@ -16,3 +16,13 @@ class Test(TestCase):
         solution = Solution()
         self.assertEqual(solution.isNumber('0.1'), True)
         self.assertEqual(solution.isNumber('0.1a'), False)
+
+
+
+    def test_e_numbers(self):
+
+        solution = Solution()
+        self.assertEqual(solution.isNumber('2e10'), True)
+        self.assertEqual(solution.isNumber('ae1'), False)
+        self.assertEqual(solution.isNumber('1.5e10'), True)
+        self.assertEqual(solution.isNumber('223'), True)
--- ValidNumber/validNumber.py
+++ ValidNumber/validNumber.py
@@ -1,23 +1,38 @@
+import re
+
 class Solution(object):
     _dot_number=0
+
+    def isN(self, s):
+        if s=='':
+            return False
+        for c in s:
+            if str.isdigit(str(c)):
+                pass
+            else:
+                return False
+        return True
+
+
+    def isDot(self, s):
+        index=s.find('.')
+        if index==-1 or index-1==len(s):
+            return self.isN(s)
+        else:
+            return self.isN(s[:index]) and self.isN(s[index+1:])
+
+    def isENumber(self, s):
+        s=s.strip()
+        index=s.find('e')
+        if index==-1 or index-1==len(s):
+            return self.isDot(s)
+        else:
+            return self.isDot(s[:index]) and self.isDot(s[index+1:])
+
+
     def isNumber(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        if s == '':
-            return False
-
-        s = s.strip()
-        if s is None:
-            return False
-        for c in s:
-            if str.isdigit(c):
-                pass
-            elif c=='.':
-                self._dot_number+=1
-                if self._dot_number>1:
-                    return False
-            else:
-                return False
-        return True
+        return self.isENumber(s)
